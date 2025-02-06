1c28d37e99ee13fcd788751512a1110d3e197a04
NBao261 <ncb2601@gmail.com>
2025-02-06 14:24:45 +0700

    update
    update

---
+++ WebApplication_06/src/java/test/UserTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import dao.UserDAO;
+import dto.UserDTO;
+
+/**
+ *
+ * @author cbao
+ */
+public class UserTest {
+
+    public static void main(String[] args) {
+        UserDAO userDAO = new UserDAO();
+
+//        //insert
+//        for (int i = 0; i < 10; i++) {
+//            UserDTO u = new UserDTO("USER" + i, "Nguyen Van " +i, "US", "_____"+i);
+//            userDAO.create(u);
+//        }
+//        UserDTO u2 = new UserDTO ("NCB01", "Nguyen Chi Bao", "AD","testupdate");
+//        userDAO.update(u2);
+        userDAO.delete("USER8");
+    }
+}
